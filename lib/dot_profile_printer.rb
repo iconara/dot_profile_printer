24e1669bed47d2b775035a8119bad3389a1d5571
Theo <theo@iconara.net>
2012-08-09 12:23:46 +0200

    Node sizes are now relative to the self time

--- lib/dot_profile_printer.rb
+++ lib/dot_profile_printer.rb
@@ -11,8 +11,9 @@ module JRuby
         super()
         @top = invocation
         @font_name = options[:font_name] || 'Menlo'
+        @base_font_size = options[:font_size] || 14
         if (renderer_class = NODE_LABEL_RENDERERS[options[:node_label_renderer] || :table])
-          @node_label_renderer = renderer_class.new
+          @node_label_renderer = renderer_class.new(self)
         else
           raise ArgumentError, %|No node label renderer for "#{options[:node_label_renderer]}"|
         end
@@ -22,7 +23,7 @@ module JRuby
         methods = method_data(@top)
 
         io.puts(GRAPH_START_DIRECTIVE)
-        io.puts(GLOBAL_NODE_DIRECTIVE % [@font_name, base_font_size, @node_label_renderer.node_shape])
+        io.puts(GLOBAL_NODE_DIRECTIVE % [@font_name, @node_label_renderer.node_shape])
         io.puts(GLOBAL_EDGE_DIRECTIVE % [@font_name, edge_font_size])
         io.puts
 
@@ -31,11 +32,12 @@ module JRuby
         methods.each do |self_serial, data|
           total_time = (data.total_time/1000000.0)
           self_time = (data.self_time/1000000.0)
-          percent_time = total_time/top_total_time * 100
           total_calls = data.total_calls
           method_name = method_name(self_serial).to_s
-          label = @node_label_renderer.render(method_name, percent_time, total_time, self_time, total_calls)
-          io.puts(NODE_DIRECTIVE_FORMAT % [self_serial, label])
+          size_modifier = 0.5 + 2 * Math.sqrt(self_time/top_total_time)
+          label = @node_label_renderer.render(size_modifier, method_name, top_total_time, total_time, self_time, total_calls)
+          font_size = (base_font_size * size_modifier).round
+          io.puts(NODE_DIRECTIVE_FORMAT % [self_serial, label, font_size])
         end
 
         io.puts
@@ -65,31 +67,55 @@ module JRuby
         io.puts(GRAPH_END_DIRECTIVE)
       end
 
+      def base_font_size
+        @base_font_size
+      end
+
+      def method_name_font_size
+        (base_font_size * 1.25).round
+      end
+
+      def edge_font_size
+        (base_font_size * 0.85).round
+      end
+
       private
 
-      NODE_DIRECTIVE_FORMAT = %|\tmethod_%-5d [label=%s];|
+      NODE_DIRECTIVE_FORMAT = %|\tmethod_%-5d [label=%s, fontsize="%d"];|
       EDGE_DIRECTIVE_FORMAT = %|\tmethod_%-5d -> method_%-5d [label="%s"];|
-      GLOBAL_NODE_DIRECTIVE = %|\tnode [fontname="%s", fontsize="14", shape="%s"];|
-      GLOBAL_EDGE_DIRECTIVE = %|\tedge [fontname="%s", fontsize="12"];|
+      GLOBAL_NODE_DIRECTIVE = %|\tnode [fontname="%s", shape="%s"];|
+      GLOBAL_EDGE_DIRECTIVE = %|\tedge [fontname="%s", fontsize="%d"];|
       GRAPH_START_DIRECTIVE = 'digraph profile {'
       GRAPH_END_DIRECTIVE   = '}'
 
-      class SimpleNodeLabelRenderer
-        TEMPLATE = '"%s\n%s\ntotal: %.3fs (%.1f%%)\nself: %.3fs\ncalls: %d"'
+      class NodeLabelRendererBase
+        def initialize(config)
+          @config = config
+        end
 
         def node_shape
           'box'
         end
 
-        def render(method_name, percent_time, total_time, self_time, total_calls)
+        def font_size(percent_time)
+          @config.base_font_size
+        end
+      end
+
+      class SimpleNodeLabelRenderer < NodeLabelRendererBase
+        TEMPLATE = '"%s\n%s\ntotal: %.3fs (%.1f%%)\nself: %.3fs (%.1f%%)\ncalls: %d"'
+
+        def render(size_modifier, method_name, top_total_time, total_time, self_time, total_calls)
           package, _, class_and_method = method_name.rpartition('::')
-          label = TEMPLATE % [package, class_and_method, total_time, percent_time, self_time, total_calls]
+          total_percent = total_time/top_total_time * 100
+          self_percent = self_time/top_total_time * 100
+          label = TEMPLATE % [package, class_and_method, total_time, total_percent, self_time, self_percent, total_calls]
           label.gsub!(/^"\\n/, '"')
           label
         end
       end
 
-      class TableNodeLabelRenderer
+      class TableNodeLabelRenderer < NodeLabelRendererBase
         TEMPLATE = %{
           <
             <TABLE BORDER="0" CELLBORDER="1" CELLSPACING="0" CELLPADDING="8">
@@ -105,7 +131,7 @@ module JRuby
                     </TR>
                     <TR>
                       <TD ALIGN="LEFT">self </TD>
-                      <TD ALIGN="RIGHT">%.3fs</TD>
+                      <TD ALIGN="RIGHT">(%.1f%%) %.3fs</TD>
                     </TR>
                     <TR>
                       <TD ALIGN="LEFT">calls </TD>
@@ -118,21 +144,24 @@ module JRuby
           >
         }.freeze
 
-        ONE_LINE_TITLE = '<FONT POINT-SIZE="18">%s</FONT><BR/>'
+        ONE_LINE_TITLE = '<FONT POINT-SIZE="%d">%s</FONT><BR/>'
         TWO_LINE_TITLE = "%s<BR/>#{ONE_LINE_TITLE}"
 
         def node_shape
           'plaintext'
         end
 
-        def render(method_name, percent_time, total_time, self_time, total_calls)
+        def render(size_modifier, method_name, top_total_time, total_time, self_time, total_calls)
+          total_percent = total_time/top_total_time * 100
+          self_percent = self_time/top_total_time * 100
           package, _, class_and_method = method_name.rpartition('::')
+          method_name_font_size = (@config.method_name_font_size * size_modifier).round
           if package.empty?
-            title = ONE_LINE_TITLE % [basic_html_escape(class_and_method)]
+            title = ONE_LINE_TITLE % [method_name_font_size, basic_html_escape(class_and_method)]
           else
-            title = TWO_LINE_TITLE % [package, basic_html_escape(class_and_method)]
+            title = TWO_LINE_TITLE % [package, method_name_font_size, basic_html_escape(class_and_method)]
           end
-          label = TEMPLATE % [title, percent_time, total_time, self_time, total_calls]
+          label = TEMPLATE % [title, total_percent, total_time, self_percent, self_time, total_calls]
           label.gsub!(/^\s*/, '')
           label.gsub!("\n", '')
           label
